149f562c3e43bfb38499aebb4e8a903eba3a3631
wenchangliu <wenchangliu@google.com>
2022-09-13 14:31:10 +0530

    SDM710-common: Codec2: Set minQP to 1 for IMS VT Call We can not reach to the target bitrate in CBR mode with default minQP when VT Call set iframe interval = -1. Set minQP=1 to generate enough bits from encoder in CBR mode.
    Bug: 155482704
    Test: reproduce steps in bug
    Change-Id: If59cf475683eb4cf8b83d02653272a1cd8294dd5
    Signed-off-by: aswin7469 <aswinas@pixysos.com>
    Signed-off-by: Cykeek <Cykeek@proton.me>

--- vendor_props.mk
+++ vendor_props.mk
@@ -188,7 +188,8 @@ PRODUCT_PROPERTY_OVERRIDES += \
     persist.vendor.radio.apm_sim_not_pwdn=1 \
     persist.vendor.radio.custom_ecc=1 \
     persist.vendor.radio.rat_on=combine \
-    persist.vendor.radio.sib16_support=1
+    persist.vendor.radio.sib16_support=1 \
+    persist.vendor.ims.mm_minqp=1
 
 # Recovery
 PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
